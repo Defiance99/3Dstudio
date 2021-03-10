d977ec57f1ba382038a66ee6cf87fabc88ef9d79
Vladimir <var.alo@mail.ru>
2021-03-10 16:04:51 +0300

    pull

---
+++ js/advantages-swiper.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let swipePaginations = document.querySelectorAll('.advatanges__swiper-pagination');
+
+    function showTActiveSlideTime(index) {
+        swipePaginations[index].classList.add('advantages__swiper-slide_active');
+    }
+
+    function hiddenPrevSlideTime(index) {
+        swipePaginations[index - 1].classList.remove('advantages__swiper-slide_active');
+    }
+
+    let swiper = new Swiper('.advantages__swiper-container', {
+        loop: true,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false
+        },
+        on: {
+            afterInit: function() {
+                showTActiveSlideTime(0);
+            }
+        }
+    });
+    
+    for (let i = 0; i < swipePaginations.length; i++) {
+        swipePaginations[i].addEventListener('click', () => {
+            swiper.slideToLoop(i);
+        });
+    }
+
+    swiper.on('realIndexChange', function() {
+        showTActiveSlideTime(swiper.realIndex);
+        hiddenPrevSlideTime(swiper.previousIndex);
+    });
+
+});
